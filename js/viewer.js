2099108fabfafa1607cd4af269cbfbf81d55208f
danieldumycz <daniel.dumycz@outlook.com>
2017-07-30 15:10:57 +1000

    UI changes, data set fixes, coordinate fixes, popup fixes

--- js/viewer.js
+++ js/viewer.js
@@ -1,5 +1,11 @@
 //GLOBALS
 var map;
+// Set up pop-up wrappers
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+var overlay = new ol.Overlay({element: container});
+
 //GLOBAL MAP TILES
 var proposedGardens = new ol.layer.Tile({
     source: new ol.source.TileWMS(({
@@ -63,11 +69,7 @@ function init(){
 
     var url = 'http://data.actmapi.act.gov.au/arcgis/rest/services/actmapi/imagery2015mga/MapServer';
     var baseUrl = 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer';
-    // Set up pop-up wrappers
-    var container = document.getElementById('popup');
-    var content = document.getElementById('popup-content');
-    var closer = document.getElementById('popup-closer');
-    var overlay = new ol.Overlay({element: container});
+    
     // Listener for the pop-up close button
     closer.onclick = function() {
         overlay.setPosition(undefined);
@@ -115,43 +117,7 @@ function init(){
     scaleLineControl.setUnits("metric");
 
     map.on('singleclick', function(evt) {
-        var wmsSource = new ol.source.TileWMS({
-            url: 'http://gis.opboomtown.com/geoserver/wms',
-            params: {'LAYERS': ['communitygardens']},
-            serverType: 'geoserver'
-        });
-        var url = wmsSource.getGetFeatureInfoUrl(
-            evt.coordinate,
-            view.getResolution(),
-            'EPSG:4326',
-            {'INFO_FORMAT': 'application/json', "feature_count": '1000'}
-        );
-    
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                var jsonResponse = JSON.parse(xhttp.responseText);
-                if(jsonResponse.features.length != 0){
-                    closer.click();
-                    content.innerHTML = '';
-                    overlay.setPosition(evt.coordinate);
-                    
-                    //Offset
-                    var extent = map.getView().calculateExtent(map.getSize())
-                    var yoffset = (extent[3] - extent[1])*0.2
-                    
-                    map.getView().animate({
-                        center: [evt.coordinate[0], evt.coordinate[1] + yoffset],
-                        duration: 500
-                    });
-                    content.innerHTML += '<img src="' + jsonResponse.features[0].properties.img + '"/>';
-                    content.innerHTML += '<b>' + jsonResponse.features[0].properties.name + '</b><br/>';
-                    content.innerHTML += '<p>' + jsonResponse.features[0].properties.description + '</p>';
-                }
-            }
-        }
-        xhttp.open("GET", url, true);
-        xhttp.send();
+        showPopup(evt);
     });
 }
 
@@ -162,13 +128,13 @@ function dropFunction() {
 
 function resetView(){
     map.getView().setCenter([149.125699, -35.284922]);
-    map.getView().setZoom(11);
+    map.getView().setZoom(12);
 }
 
 function snapTo(long, lat){
-    console.log(long + ", " + lat);
     map.getView().setCenter([long,lat]);
     map.getView().setZoom(17);
+    showPopup({'coordinate': [long, lat]});
 }
 
 function toggleFeatureVis(value){
@@ -177,4 +143,44 @@ function toggleFeatureVis(value){
     } else {
         featuresDict[value].setVisible(true);
     }
+}
+
+function showPopup(evt){
+    var wmsSource = new ol.source.TileWMS({
+        url: 'http://gis.opboomtown.com/geoserver/wms',
+        params: {'LAYERS': ['communitygardens','cite:potential_sites2']},
+        serverType: 'geoserver'
+    });
+    var url = wmsSource.getGetFeatureInfoUrl(
+        evt.coordinate,
+        map.getView().getResolution(),
+        'EPSG:4326',
+        {'INFO_FORMAT': 'application/json', "feature_count": '1000'}
+    );
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            var jsonResponse = JSON.parse(xhttp.responseText);
+            if(jsonResponse.features.length != 0){
+                closer.click();
+                content.innerHTML = '';
+                overlay.setPosition(evt.coordinate);
+                
+                //Offset
+                var extent = map.getView().calculateExtent(map.getSize())
+                var yoffset = (extent[3] - extent[1])*0.2
+                
+                map.getView().animate({
+                    center: [evt.coordinate[0], evt.coordinate[1] + yoffset],
+                    duration: 500
+                });
+                content.innerHTML += '<img src="' + jsonResponse.features[0].properties.img + '"/>';
+                content.innerHTML += '<b>' + jsonResponse.features[0].properties.name + '</b><br/>';
+                content.innerHTML += '<p>' + (jsonResponse.features[0].properties.description || (jsonResponse.features[0].properties.descriptio + jsonResponse.features[0].properties.descripti1)) + '</p>';
+            }
+        }
+    }
+    xhttp.open("GET", url, true);
+    xhttp.send();
 }
